12654783d30cda6abd6829b242110b8ded885b9d
Seigmann <simentull@gmail.com>
2026-03-06 14:30:19 +0100

    Test

--- Assets/Scrips/PlayerMovement.cs
+++ Assets/Scrips/PlayerMovement.cs
@@ -237,20 +237,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (playerCamera != null)
         {
-            playerCamera.localEulerAngles = new Vector3(pitch, 0f, 0f);
+            playerCamera.localEulerAngles = new Vector  3(pitch, 0f, 0f);
         }
     }
-<<<<<<< HEAD
-=======
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("PickUp"))
-        {
-            other.gameObject.SetActive(false);
-            count = count + 1;
-
-            SetCountText();
-        }
-    }
->>>>>>> parent of c2667dc (Level 2 + sound)
 }
