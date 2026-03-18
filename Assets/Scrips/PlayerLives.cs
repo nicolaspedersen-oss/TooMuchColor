841d1cdc0cb661a35c03ac1edd4109a6d493ff6d
Eirgro <eigr012@student.kristiania.no>
2026-03-18 16:07:28 +0100

    level 1

--- Assets/Scrips/PlayerLives.cs
+++ Assets/Scrips/PlayerLives.cs
@@ -1,16 +1,46 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerLives : MonoBehaviour
 {
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private int playerLives = 3;
+    //[SerializeField] private GameObject popupObject;
+    [SerializeField] private GameObject loseScreen;
+
+    private PlayerRespawn respawn;
+    private PlayerHealth playerDie;
+    private int livesRemaining;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        respawn = GetComponent<PlayerRespawn>();
+        playerDie = GetComponent<PlayerHealth>();
+
+        livesRemaining = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LivesLost()
+    {
+        livesRemaining--;
+        if (livesRemaining > 0)
+        {
+            respawn.Respawn();
+        }
+        else
+        {
+            playerDie.Die();
+
+            //popupObject.SetActive(true);
+            loseScreen.SetActive(true);
+            Time.timeScale = 0f;
+        }
     }
 }
